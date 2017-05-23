4f9a46b79621048fced01e31827dd4337f4a4b21
Dennis Okorie <okorie72@gmail.com>
2017-05-23 13:14:25 +0000

    Adding chef repo to git

--- chef-repo/cookbooks/apache/recipes/default.rb
+++ chef-repo/cookbooks/apache/recipes/default.rb
@@ -1,5 +1,12 @@
-#
-# Cookbook:: apache
-# Recipe:: default
-#
-# Copyright:: 2017, The Authors, All Rights Reserved.
+package 'apache' do
+       package_name httpd 
+end 
+
+service 'apache' do 
+       package_name httpd
+       action [:start, :enable]
+end
+
+file '/var/www/html/index.html' do 
+	content 'This is my main website'
+end 
